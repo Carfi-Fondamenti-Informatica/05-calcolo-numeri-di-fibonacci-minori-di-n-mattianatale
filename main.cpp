cd417a4083ff9b514eec8f3ec54e726a1bcaf21b
mattianatale <117930083+mattianatale@users.noreply.github.com>
2022-11-26 11:58:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 
 int main() {
-    int a=1;
-    int b=1;
-    int c;
     int n;
-    cin>>n;
-        for (int i=0; i<n; i++){
-            c=a;
-            a=b;
-            b=a+c;
-            if (a<=n){
-                cout<< a << endl;
-            }
+    cin >> n;
+
+        int a = 1;
+        int b = 1;
+        int c;
+
+        while (a<=n)
+        {
+            cout << a << endl;
+            c = a;
+            a = b;
+            b = a + c;
         }
    return 0;
 }
